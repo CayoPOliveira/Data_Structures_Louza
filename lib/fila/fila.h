cf429d64b0be5b1b0bfb408bcd35bd579289dd15
CayoPhellipe <cayophellipe1@gmail.com>
2022-06-23 17:00:45 -0300

    Aula4 lib update

---
+++ lib/fila/fila.h
@@ -0,0 +1,29 @@
+#ifndef FILA_H
+#define FILA_H
+
+#include "lib/lista_ligada/lista_ligada.h"
+
+// Dados
+typedef struct
+{
+  int tam;
+  No *L;
+} Fila;
+
+// Funções
+Fila *fila_criar();
+void fila_destruir(Fila **F);
+
+// Adicionar
+void fila_adicionar(Fila *F, int x);
+
+// Acessar
+int fila_topo(Fila *F);
+
+// Remover
+void fila_remover(Fila *F);
+
+// Extra
+int fila_tamanho(Fila *F);
+
+#endif
